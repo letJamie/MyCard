bbb0bfc50e76e6c9f38f501f4a2a3c4e4f244506
letJamie <stillbut@naver.com>
2020-07-23 22:45:00 +0900

    Initial Commit

---
+++ MyCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCard
+//
+//  Created by Jamie on 2020/07/23.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
